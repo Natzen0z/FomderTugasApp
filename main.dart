c4494699d92cc880cdee19ab32c55de889fdbce2
M.IrfanJanur <137706785+Natzen0z@users.noreply.github.com>
2025-11-02 23:14:38 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'todo_provider.dart';
+import 'home_page.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(create: (_) => TodoProvider(), child: const MyApp()),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
